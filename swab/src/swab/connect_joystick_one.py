dbbb45e12c2cb68c8db8206359f6fdbc7750d695
tohow06 <k82733228@gmail.com>
2020-12-20 01:51:02 +0800

    correct new package name

--- swab/src/swab/connect_joystick_one.py
+++ swab/src/swab/connect_joystick_one.py
@@ -5,6 +5,8 @@ while(1):
     p = os.popen('ls /dev/input').read().split()
     if 'js0' in p:
         p = os.popen(
-            'cd ~/catkin_ws_swab && roslaunch teapicking integral_lai.launch')
-        qq = os.popen('cd ~/catkin_ws_swab && rosrun teapicking joy_one.py')
-        q = os.popen('cd ~/catkin_ws_swab && rosrun teapicking http_lai.py')
+            'cd ~/catkin_ws_swab && roslaunch swab integral_lai.launch')
+        qq = os.popen('cd ~/catkin_ws_swab && rosrun swab joy_one.py')
+        q = os.popen('cd ~/catkin_ws_swab && rosrun swab http_lai.py')
+    else:
+	print("Can't find /dev/input/js0 ..........")
